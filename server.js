bb0d7c87d7a5f444d1b8892186fd122123a62db6
Aram-961 <aramistamboulian961@gmail.com>
2023-12-28 10:50:02 +0200

    setting up database using mongoose

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require("express");
-require("dotenv").config();
+const schema = require("./schema/schema.js");
+const color = require("colors");
+const connectDB = require("./config/db.js");
+
 const { graphqlHTTP } = require("express-graphql");
-const schema = require('./schema/schema.js')
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// connecting to database
+connectDB();
+
 app.listen(port, console.log(`server is running on port ${port}`));
-app.use("/graphql", graphqlHTTP({ 
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development' //means this will be true
-    
-})
+    graphiql: process.env.NODE_ENV === "development", //means this will be true
+  })
 );
